a23ec118556e36b5d1f3037e5d300ebff032c73e
luisedu.unifal <luisedu.unifal@gmail.com>
2022-02-07 09:37:57 -0300

    Fix import JsonSerializer

--- src/main/java/com/kafka/kafka/config/KafkaConfig.java
+++ src/main/java/com/kafka/kafka/config/KafkaConfig.java
@@ -1,10 +1,9 @@
 package com.kafka.kafka.config;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import com.kafka.kafka.model.FieldModel;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -22,7 +21,7 @@ public class KafkaConfig {
 
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:29092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
--- src/main/java/com/kafka/kafka/controller/SimpleController.java
+++ src/main/java/com/kafka/kafka/controller/SimpleController.java
@@ -1,11 +1,25 @@
 package com.kafka.kafka.controller;
 
+import com.kafka.kafka.model.FieldModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first/kafka")
 public class SimpleController {
-    
+    private KafkaTemplate<String, FieldModel> kafkaTemplate;
+
+    @Autowired
+    public SimpleController(KafkaTemplate<String, FieldModel> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public void post(@RequestBody FieldModel fieldModel) {
+        kafkaTemplate.send("testeJava", fieldModel);
+    }
 }

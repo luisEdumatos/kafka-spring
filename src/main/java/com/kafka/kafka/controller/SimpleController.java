265916c470422db671a99f1011db7e294fec8f91
luisedu.unifal <luisedu.unifal@gmail.com>
2022-02-07 09:17:38 -0300

    Fix in dependencies

---
+++ src/main/java/com/kafka/kafka/controller/SimpleController.java
@@ -0,0 +1,11 @@
+package com.kafka.kafka.controller;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first/kafka")
+public class SimpleController {
+    
+}
---
+++ src/main/java/com/kafka/kafka/model/FieldModel.java
@@ -0,0 +1,39 @@
+package com.kafka.kafka.model;
+
+import java.io.Serializable;
+
+public class FieldModel implements Serializable {
+
+    private String nome;
+    private String sobrenome;
+
+    public FieldModel() {
+
+    }
+
+    public FieldModel(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + sobrenome + "\n";
+    }
+}
